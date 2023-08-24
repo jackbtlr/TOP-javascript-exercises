b11d7544e031b7b5ca0dbfad4543ec639bcc1ab8
Jack B <16514207+jackbtlr@users.noreply.github.com>
2023-08-24 15:52:52 -0500

    Completed exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, rep) {
+    returnStr = '';
+    if(rep < 0){
+        return 'ERROR';
+    }
+    for(let i = 0; i < rep; i++){
+        returnStr += str;
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
