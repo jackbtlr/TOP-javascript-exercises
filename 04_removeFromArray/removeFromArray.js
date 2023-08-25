4c26a46369cde50448b12259b1744e6f6b3c945e
Jack B <16514207+jackbtlr@users.noreply.github.com>
2023-08-25 09:08:55 -0500

    Complete javascript exercise 4 - remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(val => {
+        return !remove.includes(val);
+    });
 };
 
 // Do not edit below this line
