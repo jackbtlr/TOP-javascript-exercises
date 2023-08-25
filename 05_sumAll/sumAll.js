397ef3ee0e1da077b3d299bfaf32aad04fa817a3
Jack B <16514207+jackbtlr@users.noreply.github.com>
2023-08-25 09:28:40 -0500

    Completed Javascript exercise 5 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    let sum = 0;
+    if(typeof start !== 'number' || typeof finish !== 'number'){
+        return 'ERROR';
+    }
+    if(start < 0 || finish < 0){
+        return 'ERROR';
+    }
+    if(start > finish){
+        let holder = finish;
+        finish = start;
+        start = holder;
+    }
+    for (let i = start; i <= finish; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
